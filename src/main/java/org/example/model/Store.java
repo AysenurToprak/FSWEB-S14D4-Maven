025441cd38db4ceea8a20a471fd0bc6dc74b8cbc
Ank <toprakaysenur96@gmail.com>
2025-01-17 20:51:06 +0300

    proje

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -2,11 +2,22 @@ package org.example.model;
 
 public class Store {
     public static void main(String[] args) {
+        ProductForSale[] products = {
+                new Chocolate("Tablet", 350, "sweet", "White" ),
+                new Coke("Bottle", 45, "cherry coke", true),
+                new Bread("Baguette", 25, "French style", "Wheat")
+        };
 
-        //listProducts();
+        Store store = new Store();
+        store.listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
+        for (ProductForSale product : products) {
+            if (product != null) {
+                product.showDetails();
+            }
+        }
 
     }
 }
